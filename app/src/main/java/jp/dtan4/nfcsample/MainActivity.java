58e1ea64c6f907b6a18ce4eb94272b259e5a44de
dtan4 <dtanshi45@gmail.com>
2014-07-17 15:12:25 +0900

    Show ID of scanned card

--- app/src/main/java/jp/dtan4/nfcsample/MainActivity.java
+++ app/src/main/java/jp/dtan4/nfcsample/MainActivity.java
@@ -1,19 +1,23 @@
 package jp.dtan4.nfcsample;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +28,68 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter != null) {
+            if (!nfcAdapter.isEnabled()) {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_nfc_disable),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.error_nfc_nosupport),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (nfcAdapter != null) {
+            // Set priority to get NFC
+            Intent intent = new Intent(this, this.getClass()).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            PendingIntent pendingIntent =
+                    PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    private static String toHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * 2);
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(b & 0xff);
+            if (hex.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(hex);
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            byte[] idm = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+            TextView tvRead = (TextView)findViewById(R.id.read);
+            tvRead.setText(toHexString(idm));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
